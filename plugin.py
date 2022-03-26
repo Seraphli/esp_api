d4c9c3948820cc1399c57f1d4e989a2c11d94a3e
SErAphLi <seraphlivery@gmail.com>
2022-03-27 01:43:46 +0800

    fix: update api

--- plugin.py
+++ plugin.py
@@ -32,60 +32,65 @@ class PluginApi(socketio.AsyncClientNamespace):
     def on_echo(self, data):
         print("Echo:", data)
 
-    def on_register_topic(self, data):
-        print("Register topic:", data)
-
-    def on_add_input_hook(self, data):
+    def on_addInputHook(self, data):
         print("Add input hook:", data)
 
-    def on_del_input_hook(self, data):
+    def on_delInputHook(self, data):
         print("Del input hook:", data)
 
-    def on_insert_css(self, data):
+    def on_insertCSS(self, data):
         print("Insert css:", data)
 
-    def on_remove_css(self, data):
+    def on_removeCSS(self, data):
         print("Remove css:", data)
 
-    def on_update_elem(self, data):
-        print("Update elem:", data)
+    def on_addElem(self, data):
+        print("Add elem:", data)
+        self.elem_count += 1
 
-    def on_remove_elem(self, data):
+    def on_delElem(self, data):
         print("Remove elem:", data)
+        self.elem_count -= 1
 
-    def on_show_view(self, data):
+    def on_showElem(self, data):
         print("Show view:", data)
 
-    def on_hide_view(self, data):
+    def on_hideElem(self, data):
         print("Hide view:", data)
 
-    def on_exec_js_in_elem(self, data):
+    def on_setBound(self, data):
+        print("Set bound:", data)
+
+    def on_setContent(self, data):
+        print("Set content:", data)
+
+    def on_setOpacity(self, data):
+        print("Set opacity:", data)
+
+    def on_execJSInElem(self, data):
         print("Exec js in elem:", data)
 
     def on_notify(self, data):
         print("Notify:", data)
 
-    def on_update_bound(self, key, _type, bound):
-        print("Update bound:", key, _type, bound)
+    def on_updateBound(self, key, bound):
+        print("Update bound:", key, bound)
 
-    async def on_process_content(self, content):
+    def on_updateOpacity(self, key, opacity):
+        print("Update opacity:", key, opacity)
+
+    async def on_processContent(self, content):
         print("Process content:", content)
         await self.parent.process_content(content)
 
-    def on_mode_flag(self, lock_flag, move_flag, dev_flag):
-        print("Mode flag:", lock_flag, move_flag, dev_flag)
+    def on_modeFlag(self, flags):
+        print("Mode flag:", flags)
 
-    def on_elem_activated(self, key):
-        print("Elem activated:", key)
-
-    def on_elem_deactivated(self, key):
-        print("Elem deactivated:", key)
-
-    def on_elem_remove(self, key):
+    def on_elemRemove(self, key):
         print("Elem remove:", key)
         return False
 
-    def on_elem_refresh(self, key):
+    def on_elemRefresh(self, key):
         print("Elem refresh:", key)
         return False
 
@@ -94,74 +99,53 @@ class APIHandler(object):
     def __init__(self, parent) -> None:
         self.parent = parent
         self.elems = {}
-    
-    @property
-    def ctx(self):
-        return self.parent.ctx
 
     def new_basic(self, content):
-        key = f"basic-{len(self.elems)}"
+        catKey = f"api-{len(self.elems)}"
         basic = {
-            "key": key,
             "type": 0,
             "bound": {"x": -1, "y": -1, "w": -1, "h": -1},
             "content": content,
         }
-        self.elems[key] = basic
-        return basic
+        self.elems[catKey] = basic
+        return catKey, basic
 
     def new_view(self, content):
-        key = f"view-{len(self.elems)}"
+        catKey = f"api-{len(self.elems)}"
         view = {
-            "key": key,
             "type": 1,
             "bound": {"x": -1, "y": -1, "w": -1, "h": -1},
             "content": content,
         }
-        self.elems[key] = view
-        return view
+        self.elems[catKey] = view
+        return catKey, view
 
-    async def api_process_content(self, data):
+    async def api_processContent(self, data):
         print("Process content:", data)
         html = re.compile(r"<[^>]+>")
-        css = re.compile(
-            r"([#.@]?[\w.:> ]+)[\s]{[ ]*[\r\n]*([A-Za-z\-\r\n\t]+[:][\s]*[\w ./()\-!,]+;[\r\n]*(?:[A-Za-z\- \r\n\t]+[:][\s]*[\w ./()\-!,]+;[\r\n]*)*)[ ]*}"
-        )
         http = re.compile(
             r"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\w]*))?)"
         )
         if html.match(data):
             await sio.emit(
-                "update_elem",
-                data=(
-                    self.ctx,
-                    self.new_basic(data),
-                ),
+                "addElem",
+                data=(*self.new_basic(data),),
             )
         elif http.match(data):
             await sio.emit(
-                "update_elem",
-                data=(
-                    self.ctx,
-                    self.new_view(data),
-                ),
+                "addElem",
+                data=(*self.new_view(data),),
             )
-        elif css.match(data):
-            await sio.emit("insert_css", data=(self.ctx, data))
         else:
             await sio.emit(
-                "update_elem",
-                data=(
-                    self.ctx,
-                    self.new_basic(f'<div class="card">{data}</div>'),
-                ),
+                "addElem",
+                data=(*self.new_basic(f'<div class="card">{data}</div>'),),
             )
 
     async def api_notify(self, data):
         await sio.emit(
             "notify",
             data=(
-                self.ctx,
                 {
                     "text": data,
                     "title": PLUGIN_NAME,
@@ -169,40 +153,41 @@ class APIHandler(object):
             ),
         )
 
-    async def api_insert_css(self, data):
-        await sio.emit("insert_css", data=(self.ctx, data))
+    async def api_insertCSS(self, data):
+        catKey, css = data.split("|")
+        await sio.emit("insertCSS", data=(catKey, css))
 
-    async def api_remove_css(self, data):
-        await sio.emit("remove_css", data=(self.ctx, data))
+    async def api_removeCSS(self, data):
+        catKey, cssKey = data.split("|")
+        await sio.emit("removeCSS", data=(catKey, cssKey))
 
-    async def api_remove_elem(self, data):
-        key = data
-        if key not in self.elems:
+    async def api_delElem(self, data):
+        catKey = data
+        if catKey not in self.elems:
             return
-        await sio.emit("remove_elem", data=(self.ctx, data))
+        await sio.emit("delElem", data=(catKey))
 
-    async def api_show_view(self, data):
-        key = data
-        if key not in self.elems or self.elems[key]["type"] != 1:
+    async def api_showElem(self, data):
+        catKey = data
+        if catKey not in self.elems:
             return
-        await sio.emit("show_view", data=(self.ctx, self.elems[key]))
+        await sio.emit("showElem", data=(catKey))
 
-    async def api_hide_view(self, data):
-        key = data
-        if key not in self.elems or self.elems[key]["type"] != 1:
+    async def api_hideElem(self, data):
+        catKey = data
+        if catKey not in self.elems:
             return
-        await sio.emit("hide_view", data=(self.ctx, self.elems[key]))
+        await sio.emit("hideElem", data=(catKey))
 
     async def api_js(self, data):
-        key, cmd = data.split(" ")
-        if key not in self.elems:
+        catKey, code = data.split("|")
+        if catKey not in self.elems:
             return
         await sio.emit(
-            "exec_js_in_elem",
+            "execJSInElem",
             data=(
-                self.ctx,
-                self.elems[key],
-                cmd,
+                catKey,
+                code,
             ),
         )
 
@@ -240,32 +225,20 @@ class Plugin(object):
                     if attr.startswith("api_") and attr.endswith(api):
                         await getattr(self.handler, attr)(data)
                         return
-                await self.handler.api_process_content(data)
+                await self.handler.api_processContent(data)
             else:
-                await self.handler.api_process_content(content)
+                await self.handler.api_processContent(content)
         except:
             import traceback
+
             traceback.print_exc()
-
-    async def register(self):
-        # Create a context for registering plugins
-        # You can either use sample password or use complex password
-        # You can also register multiple topic
-        ctx = {"topic": SHORT_NAME, "pwd": str(uuid.uuid4())}
-        await sio.emit("register_topic", ctx)
-        self.ctx = ctx
-
-    async def wait_for_elem(self):
-        while self.api.elem_count < 2:
-            await asyncio.sleep(0.1)
 
     async def test_case(self):
         # get input 'foo' from like 'g foo'
-        await sio.emit("add_input_hook", data=(self.ctx, self.cfg["input_hook"]))
+        await sio.emit("addInputHook", data=(self.cfg["input_hook"]))
 
     async def loop(self):
         await sio.connect(f"http://localhost:{self.port}")
-        await self.register()
         await self.test_case()
         await sio.wait()
 
